409947335ba71a5d668758cc23650b692fdb50b4
genom <genomvpn@gmail.com>
2025-05-18 13:29:54 +0300

    lab #5

--- laba5.py
+++ laba5.py
@@ -7,8 +7,8 @@ from PyQt5.QtWidgets import QWidget
 class MAIN(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.resize(QSize(800,500))
-        self.setMinimumSize(500,400)
+        # self.resize(QSize(800,500))
+        # self.setMinimumSize(500,400)
         lay = QVBoxLayout()
 
         self.Label = QLabel()
@@ -17,13 +17,16 @@ class MAIN(QMainWindow):
         lay.addWidget(self.line)
         lay.addWidget(self.Label)
         lay.addWidget(self.button)
-        self.button.setFixedSize(200,200)
+        self.Label.setFixedHeight(50)
+        self.button.setFixedHeight(100)
         con = QWidget()
         con.setLayout(lay)
         self.setCentralWidget(con)
+        self.button.clicked.connect(self.clickk)
+        self.setGeometry(300, 300, 500, 400)
+    def clickk(self):
+        self.Label.setText(self.line.text())
 
-        # self.setCentralWidget(LAbel)
-        # self.setCentralWidget(line)
 
 
 
